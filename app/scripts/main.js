f58d4f49c0a52557c81ba3d9b8b11c90319517bb
PinkyJie <pilixiaoxuanfeng@gmail.com>
2013-12-31 14:10:44 +0800

    add affix affect for navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 (function($){
     'use strict';
     function init() {
+        // for chinese/english switch
         $('li.inactive img')
             .mouseenter(function(){
                 $(this).removeClass('gray-img');
@@ -8,6 +9,20 @@
             .mouseleave(function(){
                 $(this).addClass('gray-img');
             });
+        // for affix navbar
+        $(window).scroll(function(){
+            var navAnchor = $('.cv-title').offset().top;
+            if ($(this).scrollTop() > navAnchor && $('.nav').css('position') !== 'fixed') {
+                $('.nav').css({
+                    'position': 'fixed',
+                    'top': '-1px'
+                }).addClass('splited');
+            } else if ($(this).scrollTop() < navAnchor && $('.nav').css('position') !== 'relative') {
+                $('.nav').css({
+                    'position': 'relative'
+                }).removeClass('splited');
+            }
+        });
     }
 
     $(init);
